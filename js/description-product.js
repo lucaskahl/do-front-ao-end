f0efb6f3810b0e5eb6ccc257897ad379abffff71
Lucas Kahl <lucaschangkahl@gmail.com>
2019-01-07 22:52:49 +0000

    Desafio 001 - Aula 013

--- js/description-product.js
+++ js/description-product.js
@@ -1,4 +1,7 @@
 const $heart = document.querySelector(".-heart");
+const $secondButton = document.querySelector(".button-store.-second");
+
+console.log($secondButton);
 
 $heart.addEventListener("click", handleClick);
 
