dcf497c195dfce73ebc99a6fc0ba17a7c5cec61c
Lucas Kahl <lucaschangkahl@gmail.com>
2019-01-08 23:01:13 +0000

    Aula 014 - Desafio 002

--- js/description-product.js
+++ js/description-product.js
@@ -1,7 +1,15 @@
 const $heart = document.querySelector(".-heart");
+const $stars = document.querySelectorAll(".-stars");
 
-$heart.addEventListener("click", handleClick);
+//$heart.addEventListener("click", handleClick);
+$stars.forEach(element => {
+  element.addEventListener("click", handleClick);
+});
 
 function handleClick() {
-  $heart.classList.toggle("-active");
+  if (this.src == "file:///E:/Projetos/lojafone/img/star.png") {
+    this.src = "file:///E:/Projetos/lojafone/img/star-active.png";
+  } else {
+    this.src = "file:///E:/Projetos/lojafone/img/star.png";
+  }
 }
