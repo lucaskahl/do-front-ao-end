9dad3f31d6f713259bbd2ac0dcfe54709fca594f
Lucas Kahl <lucaschangkahl@gmail.com>
2019-01-08 23:20:26 +0000

    Aula 014 - Desafio 003 FAILED

--- js/description-product.js
+++ js/description-product.js
@@ -1,15 +1,18 @@
 const $heart = document.querySelector(".-heart");
 const $stars = document.querySelectorAll(".-stars");
 
-//$heart.addEventListener("click", handleClick);
-$stars.forEach(element => {
-  element.addEventListener("click", handleClick);
+$heart.addEventListener("click", handleClick);
+
+$stars.forEach((value, index) => {
+  console.log(index);
 });
 
 function handleClick() {
-  if (this.src == "file:///E:/Projetos/lojafone/img/star.png") {
-    this.src = "file:///E:/Projetos/lojafone/img/star-active.png";
-  } else {
-    this.src = "file:///E:/Projetos/lojafone/img/star.png";
-  }
+  // if (this.src == "file:///E:/Projetos/lojafone/img/star.png") {
+  //   this.src = "file:///E:/Projetos/lojafone/img/star-active.png";
+  // } else {
+  //   this.src = "file:///E:/Projetos/lojafone/img/star.png";
+  // }
+
+  $heart.classList.toggle("-active");
 }
