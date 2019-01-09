67e2102dfe484451647e451f3ca9c739ef6462dc
Lucas Kahl <lucaschangkahl@gmail.com>
2019-01-09 22:58:38 +0000

    Aula 015

--- js/description-product.js
+++ js/description-product.js
@@ -1,18 +1,12 @@
 const $heart = document.querySelector(".-heart");
-const $stars = document.querySelectorAll(".-stars");
+const $stars = document.querySelectorAll(".star");
 
 $heart.addEventListener("click", handleClick);
 
-$stars.forEach((value, index) => {
-  console.log(index);
+$stars.forEach(($star, key) => {
+  $star.addEventListener("click", handleClick);
 });
 
 function handleClick() {
-  // if (this.src == "file:///E:/Projetos/lojafone/img/star.png") {
-  //   this.src = "file:///E:/Projetos/lojafone/img/star-active.png";
-  // } else {
-  //   this.src = "file:///E:/Projetos/lojafone/img/star.png";
-  // }
-
-  $heart.classList.toggle("-active");
+  this.classList.toggle("-active");
 }
