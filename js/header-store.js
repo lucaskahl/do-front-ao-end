1ad68f7632519ebb1c0c5ac0cf789548f4da6e38
Lucas Kahl <lucaschangkahl@gmail.com>
2019-01-15 22:22:43 +0000

    Desafio hard - 017

--- js/header-store.js
+++ js/header-store.js
@@ -1,12 +1,15 @@
 const $search = document.querySelector("[type='search']");
 const $searchIcon = document.querySelector(".search-icon");
+const $xIcon = document.querySelector(".x-icon");
 
 $searchIcon.addEventListener("click", () => {
   $searchIcon.classList.toggle("-active");
   $search.classList.toggle("-active");
+  $xIcon.classList.toggle("-active");
 });
 
-$search.addEventListener("click", () => {
+$xIcon.addEventListener("click", () => {
   $searchIcon.classList.toggle("-active");
   $search.classList.toggle("-active");
+  $xIcon.classList.toggle("-active");
 });
