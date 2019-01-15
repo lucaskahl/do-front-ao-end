8733fc8ecb142ad8972854643335878db4377e82
Lucas Kahl <lucaschangkahl@gmail.com>
2019-01-15 22:16:29 +0000

    Desafio Intermediario- Aula 017

--- js/header-store.js
+++ js/header-store.js
@@ -1,5 +1,12 @@
 const $search = document.querySelector("[type='search']");
+const $searchIcon = document.querySelector(".search-icon");
+
+$searchIcon.addEventListener("click", () => {
+  $searchIcon.classList.toggle("-active");
+  $search.classList.toggle("-active");
+});
 
 $search.addEventListener("click", () => {
+  $searchIcon.classList.toggle("-active");
   $search.classList.toggle("-active");
 });
