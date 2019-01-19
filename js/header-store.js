8cdfcc84b068e2026ba17a9c4dee675f165f1e68
Lucas Kahl <lucaschangkahl@gmail.com>
2019-01-19 00:48:36 +0000

    Refactor aula 019

--- js/header-store.js
+++ js/header-store.js
@@ -1,6 +1,6 @@
-const $menuMobile = document.querySelector(".menu-mobile");
-const $menu = $menuMobile.querySelector("#menu");
+// const $menuMobile = document.querySelector(".menu-mobile");
+// const $menu = $menuMobile.querySelector("#menu");
 
-$menuMobile.addEventListener("click", () => {
-  $menu.classList.toggle("-active");
-});
+// $menuMobile.addEventListener("click", () => {
+//   $menu.classList.toggle("-active");
+// });
